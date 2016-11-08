0b039303f0aba85a2868e9353d7367646b9811d9
Steve Kondik <steve@cyngn.com>
2016-11-08 12:54:58 -0800

    nx611j: Build with SDCLANG if available
    Change-Id: I76b21bd40ea0d4b29dec98e75ebf13ebe479752a
    (cherry picked from commit d0a0e82458d577a66830258bf65f644f132711ae)

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -23,6 +23,9 @@
 
 DEVICE_PATH := device/nubia/nx611j
 
+# Use Snapdragon LLVM, if available
+TARGET_USE_SDCLANG := true
+
 # Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := sdm660
 TARGET_NO_BOOTLOADER := true
