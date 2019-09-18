fdaf51a4b4ed4f8e3e305b1067c184afa529ea34
Cyborg2017 <wangyang0987654@gmail.com>
2019-09-19 04:33:28 +0800

    nx611j: Drop unused XiaomiDoze

--- device.mk
+++ device.mk
@@ -167,10 +167,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.configstore@1.0-service
 
-# Doze
-PRODUCT_PACKAGES += \
-    XiaomiDoze
-
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
