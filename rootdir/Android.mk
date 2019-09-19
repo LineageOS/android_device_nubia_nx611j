86a7c4ba7c681b9d9f4da49022e8640dcd22a89e
Cyborg2017 <wangyang0987654@gmail.com>
2019-09-19 16:59:51 +0800

    nx611j: rootdir: Import init.nb.sensor.rc from stock firmware
    * nubia/NX611J/NX611J:8.1.0/OPM1.171019.011/nubia.20190820.192926:user/release-keys

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -99,3 +99,11 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/fstab.qcom
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)
 include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.nb.sensor.rc
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.nb.sensor.rc
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
+include $(BUILD_PREBUILT)
