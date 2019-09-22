c18d9cdc12e9589b4420767dcd4be22376abe790
Quallenauge <Hamsi2k@freenet.de>
2019-09-22 21:28:13 +0200

    nx611j: libshims: Respect non-static member function calls.
    As stated in
    https://www.learncpp.com/cpp-tutorial/8-8-the-hidden-this-pointer/
    member functions contains a hidden parameter (a pointer of the current
    reference of teh object, aka "this"). This way the code knows in which
    context the member function is called. We have to resprect that behavior
    into our shims which otherwise crashes or lead to memory corruption.
    
    Change-Id: I68197ebafde8773fd73d7dafb9b2f1bae92ee170
    (cherry picked from commit 92c6b47542e11627c635e6ddeb3dab6dd4e36322)

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -15,7 +15,7 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_SRC_FILES := camera_shim.cpp
+LOCAL_SRC_FILES := camera_sdm660_shim.cpp
 LOCAL_SHARED_LIBRARIES := libui libutils
 LOCAL_MODULE := libshim_camera
 LOCAL_MODULE_TAGS := optional
