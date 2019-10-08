5296ccf1696d35215457ca05a96d5b6fa2942459
Cyborg2017 <wangyang0987654@gmail.com>
2019-10-08 09:25:39 +0800

    nx611j: Remove binder lights Hal

--- device.mk
+++ device.mk
@@ -262,8 +262,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/gpio-keys.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/gpio-keys.kl
 
 # Lights
-PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-service.nubia_nx611j
 
 # LiveDisplay native
 PRODUCT_PACKAGES += \
