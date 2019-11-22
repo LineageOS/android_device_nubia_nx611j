2f10200823b4fd8a4cbdc3c54b4d3fec1540b1e6
Mikhail Naganov <mnaganov@google.com>
2019-11-22 10:56:29 -0800

    nx611j: Switch to Audio HAL V6
    Switch to Audio HAL V6
    
    Bug: 141989952
    Test: atest VtsHalAudioV6_0TargetTest
          atest VtsHalAudioEffectV6_0TargetTest
                manual audio "smoke" test on taimen and walleye
    
    Change-Id: Ieb1cc5c7fdcc5416a328a0a5c4a1996258b81ac8
    (cherry picked from commit 939fd6b3019828b56469d7b75f237bf176bfe57f)

--- device.mk
+++ device.mk
@@ -83,9 +83,9 @@ PRODUCT_COPY_FILES += \
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@5.0-impl:32 \
+    android.hardware.audio@6.0-impl \
     android.hardware.audio@2.0-service \
-    android.hardware.audio.effect@5.0-impl:32 \
+    android.hardware.audio.effect@6.0-impl \
     android.hardware.soundtrigger@2.2-impl:32 \
     audio.a2dp.default \
     audio.primary.sdm660 \
