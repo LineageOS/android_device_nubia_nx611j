00831dc63b7e9946f580a0a8b7faa6be45729490
Erfan Abdi <erfangplus@gmail.com>
2020-03-07 04:30:48 +0330

    nx611j: Move all props to properties.mk
    * Also add more, drop unused props
    
    Change-Id: I4cf605b6f6a5e5fcf05e84ceb30497bb7a29cfab
    (cherry picked from commit aab2a1c55025ce138d4f68aa9f04b78d21f6770e)

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -1,180 +1,166 @@
+# Audio
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.sys.job_delay=true \
-    persist.sys.mcd_config_file=/system/etc/mcd_default.conf \
-    video.disable.ubwc=1 \
-    vendor.video.disable.ubwc=1 \
-    dalvik.vm.heapgrowthlimit=256m \
-    dalvik.vm.heapstartsize=8m \
-    dalvik.vm.heapsize=512m \
-    dalvik.vm.heaptargetutilization=0.75 \
-    dalvik.vm.heapminfree=512k \
-    dalvik.vm.heapmaxfree=8m \
-    persist.radio.multisim.config=dsds \
-    persist.vendor.qcomsysd.enabled=1 \
-    ro.com.android.dataroaming=true \
-    ro.vendor.extension_library=libqti-perfd-client.so \
-    persist.vendor.radio.apm_sim_not_pwdn=1 \
-    persist.vendor.radio.sib16_support=1 \
-    persist.vendor.radio.custom_ecc=1 \
-    persist.vendor.radio.rat_on=combine \
-    persist.radio.schd.cache=3500 \
-    sys.vendor.shutdown.waittime=500 \
-    ro.build.shutdown_timeout=0 \
-    ro.frp.pst=/dev/block/bootdevice/by-name/frp \
-    ro.opengles.version=196610 \
-    vendor.qcom.bluetooth.soc=cherokee \
     af.fast_track_multiplier=1 \
     vendor.audio_hal.period_size=192 \
     ro.vendor.audio.sdk.fluencetype=fluence \
     persist.vendor.audio.fluence.voicecall=true \
-    persist.vendor.audio.fluence.voicerec=true \
+    persist.vendor.audio.fluence.voicerec=false \
     persist.vendor.audio.fluence.speaker=true \
-    persist.vendor.audio.fluence.audiorec=true \
     vendor.audio.tunnel.encode=false \
-    persist.vendor.audio.ras.enabled=true \
+    persist.vendor.audio.ras.enabled=false \
     vendor.audio.offload.buffer.size.kb=64 \
     audio.offload.min.duration.secs=30 \
     audio.offload.video=true \
     vendor.audio.offload.track.enable=true \
     audio.deep_buffer.media=true \
     vendor.voice.path.for.pcm.voip=true \
-    vendor.audio.feature.compr_voip.enable=true \
-    vendor.audio.feature.kpi_optimize.enable=true \
-    vendor.audio.feature.fluence.enable=true \
-    vendor.audio.feature.custom_stereo.enable=true \
-    vendor.audio.feature.anc_headset.enable=true \
-    vendor.audio.feature.vbat.enabl=true \
-    vendor.audio.feature.fm.enable=true \
     vendor.audio.offload.multiaac.enable=true \
     vendor.audio.dolby.ds2.enabled=false \
     vendor.audio.dolby.ds2.hardbypass=false \
     vendor.audio.offload.multiple.enabled=false \
     vendor.audio.offload.passthrough=false \
     ro.vendor.audio.sdk.ssr=false \
-    ro.config.media_vol_steps=25 \
-    ro.config.vc_call_vol_steps=7 \
     vendor.audio.offload.gapless.enabled=true \
     vendor.audio.safx.pbe.enabled=false \
     vendor.audio.parser.ip.buffer.size=262144 \
     vendor.audio.flac.sw.decoder.24bit=true \
-    persist.vendor.bt.a2dp_offload_cap=sbc-aac \
     vendor.audio.use.sw.alac.decoder=true \
     vendor.audio.use.sw.ape.decoder=true \
     vendor.audio.hw.aac.encoder=true \
-    vendor.fm.a2dp.conc.disabled=true \
     audio.sys.noisy.broadcast.delay=600 \
-    persist.vendor.audio.hifi.int_codec=true \
     audio.sys.offload.pstimeout.secs=3 \
-    vendor.audio.adm.buffering.ms=3 \
-    ro.hardware.fingerprint=fpc \
-    ro.vendor.sensors.facing=false \
-    ro.vendor.sensors.cmc=false \
-    ro.vendor.sdk.sensors.gestures=false \
-    rild.libpath=/system/vendor/lib64/libril-qc-qmi-1.so \
-    sys.fp.miui.token=0 \
-    persist.camera.stats.test=5 \
-    persist.camera.HAL3.enabled=1
-
-#enable headset calibration
-PRODUCT_PROPERTY_OVERRIDES += \
-vendor.audio.volume.headset.gain.depcal=true
-
-#add dynamic feature flags here
-PRODUCT_PROPERTY_OVERRIDES += \
-vendor.audio.feature.afe_proxy.enable=true \
-vendor.audio.feature.anc_headset.enable=true \
-vendor.audio.feature.battery_listener.enable=false \
-vendor.audio.feature.compr_cap.enable=false \
-vendor.audio.feature.compress_in.enable=false \
-vendor.audio.feature.compress_meta_data.enable=true \
-vendor.audio.feature.compr_voip.enable=true \
-vendor.audio.feature.concurrent_capture.enable=false \
-vendor.audio.feature.custom_stereo.enable=true \
-vendor.audio.feature.display_port.enable=true \
-vendor.audio.feature.dsm_feedback.enable=false \
-vendor.audio.feature.dynamic_ecns.enable=false \
-vendor.audio.feature.ext_hw_plugin.enable=false \
-vendor.audio.feature.external_dsp.enable=false \
-vendor.audio.feature.external_speaker.enable=false \
-vendor.audio.feature.external_speaker_tfa.enable=false \
-vendor.audio.feature.fluence.enable=true \
-vendor.audio.feature.fm.enable=true \
-vendor.audio.feature.hdmi_edid.enable=true \
-vendor.audio.feature.hdmi_passthrough.enable=true \
-vendor.audio.feature.hfp.enable=true \
-vendor.audio.feature.hifi_audio.enable=true \
-vendor.audio.feature.hwdep_cal.enable=false \
-vendor.audio.feature.incall_music.enable=false \
-vendor.audio.feature.multi_voice_session.enable=true \
-vendor.audio.feature.keep_alive.enable=false \
-vendor.audio.feature.kpi_optimize.enable=true \
-vendor.audio.feature.maxx_audio.enable=false \
-vendor.audio.feature.ras.enable=true \
-vendor.audio.feature.record_play_concurency.enable=false \
-vendor.audio.feature.src_trkn.enable=true \
-vendor.audio.feature.spkr_prot.enable=true \
-vendor.audio.feature.ssrec.enable=true \
-vendor.audio.feature.usb_offload.enable=true \
-vendor.audio.feature.usb_offload_burst_mode.enable=false \
-vendor.audio.feature.usb_offload_sidetone_volume.enable=false \
-vendor.audio.feature.deepbuffer_as_primary.enable=false \
-vendor.audio.feature.vbat.enable=true \
-vendor.audio.feature.wsa.enable=false \
-vendor.audio.feature.audiozoom.enable=false \
-vendor.audio.feature.snd_mon.enable=true
-
-#enable dualmic fluence for voice communication
-PRODUCT_PROPERTY_OVERRIDES += \
-persist.audio.fluence.voicecomm=true
-
-#enable AAC frame ctl for A2DP sinks
-PRODUCT_PROPERTY_OVERRIDES += \
-persist.vendor.bt.aac_frm_ctl.enabled=true
-
-#Set speaker protection cal tx path sampling rate to 48k
-PRODUCT_PROPERTY_OVERRIDES += \
-vendor.audio.spkr_prot.tx.sampling_rate=48000
-
-#Set AudioFlinger client heap size
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.af.client_heap_size_kbyte=7168
+    persist.vendor.audio.hifi.int_codec=true \
+    ro.af.client_heap_size_kbyte=7168 \
+    vendor.audio.volume.headset.gain.depcal=true \
+    persist.vendor.bt.aac_frm_ctl.enabled=true \
+    vendor.audio.spkr_prot.tx.sampling_rate=48000
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    vendor.audio_hal.in_period_size=144 \
-    vendor.audio_hal.period_multiplier=3 \
-    vendor.audio.adm.buffering.ms=2
+    vendor.audio.feature.afe_proxy.enable=true \
+    vendor.audio.feature.anc_headset.enable=true \
+    vendor.audio.feature.battery_listener.enable=false \
+    vendor.audio.feature.compr_cap.enable=false \
+    vendor.audio.feature.compress_in.enable=false \
+    vendor.audio.feature.compress_meta_data.enable=true \
+    vendor.audio.feature.compr_voip.enable=true \
+    vendor.audio.feature.concurrent_capture.enable=false \
+    vendor.audio.feature.custom_stereo.enable=true \
+    vendor.audio.feature.display_port.enable=true \
+    vendor.audio.feature.dsm_feedback.enable=false \
+    vendor.audio.feature.dynamic_ecns.enable=false \
+    vendor.audio.feature.ext_hw_plugin.enable=false \
+    vendor.audio.feature.external_dsp.enable=false \
+    vendor.audio.feature.external_speaker.enable=false \
+    vendor.audio.feature.external_speaker_tfa.enable=false \
+    vendor.audio.feature.fluence.enable=true \
+    vendor.audio.feature.fm.enable=true \
+    vendor.audio.feature.hdmi_edid.enable=true \
+    vendor.audio.feature.hdmi_passthrough.enable=true \
+    vendor.audio.feature.hfp.enable=true \
+    vendor.audio.feature.hifi_audio.enable=true \
+    vendor.audio.feature.hwdep_cal.enable=false \
+    vendor.audio.feature.incall_music.enable=false \
+    vendor.audio.feature.multi_voice_session.enable=true \
+    vendor.audio.feature.keep_alive.enable=false \
+    vendor.audio.feature.kpi_optimize.enable=true \
+    vendor.audio.feature.maxx_audio.enable=false \
+    vendor.audio.feature.ras.enable=true \
+    vendor.audio.feature.record_play_concurency.enable=false \
+    vendor.audio.feature.src_trkn.enable=true \
+    vendor.audio.feature.spkr_prot.enable=true \
+    vendor.audio.feature.ssrec.enable=true \
+    vendor.audio.feature.usb_offload.enable=true \
+    vendor.audio.feature.usb_offload_burst_mode.enable=false \
+    vendor.audio.feature.usb_offload_sidetone_volume.enable=false \
+    vendor.audio.feature.deepbuffer_as_primary.enable=false \
+    vendor.audio.feature.vbat.enable=true \
+    vendor.audio.feature.wsa.enable=false \
+    vendor.audio.feature.audiozoom.enable=false \
+    vendor.audio.feature.snd_mon.enable=true
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.vendor.audio.hw.binder.size_kbyte=1024
+    ro.config.media_vol_steps=25 \
+    ro.config.vc_call_vol_steps=7 \
+    vendor.audio.adm.buffering.ms=6
 
-# HAL1 apps list
+# Bluetooth
 PRODUCT_PROPERTY_OVERRIDES += \
-    vendor.camera.hal1.packagelist=com.whatsapp,com.intsig.camscanner,com.instagram.android
+    vendor.qcom.bluetooth.soc=cherokee
 
-# Debug Options
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.dbg.ims_volte_enable=1 \
-    persist.dbg.volte_avail_ovr=1 \
-    persist.dbg.vt_avail_ovr=1 \
-    persist.dbg.wfc_avail_ovr=1
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    vendor.bluetooth.soc=cherokee
 
-# Radio Options
+# Camera
 PRODUCT_PROPERTY_OVERRIDES += \
+    persist.vendor.camera.preview.ubwc=0 \
+    vendor.video.disable.ubwc=1 \
+    vidc.enc.dcvs.extra-buff-count=2
+
+# CNE
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    persist.vendor.cne.feature=1
+
+# Display
+PRODUCT_PROPERTY_OVERRIDES += \
+    debug.sf.enable_hwc_vds=1 \
+    debug.sf.hw=1 \
+    debug.sf.latch_unsignaled=1 \
+    ro.opengles.version=196610 \
+    ro.vendor.display.cabl=0 \
+    vendor.display.disable_skip_validate=1 \
+    vendor.display.enable_default_color_mode=1 \
+    vendor.gralloc.enable_fb_ubwc=1
+
+# FRP
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.frp.pst=/dev/block/bootdevice/by-name/frp
+
+# Media
+PRODUCT_PROPERTY_OVERRIDES += \
+    media.stagefright.thumbnail.prefer_hw_codecs=true \
+    vendor.vidc.dec.enable.downscalar=1 \
+    vendor.vidc.enc.disable_bframes=1
+
+# Netflix
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.netflix.bsp_rev=Q660-13149-1
+
+# Perf
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.extension_library=libqti-perfd-client.so
+
+# Radio
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.radio.multisim.config=dsds \
+    persist.vendor.data.mode=concurrent \
+    persist.vendor.radio.apm_sim_not_pwdn=1 \
+    persist.vendor.radio.atfwd.start=true \
+    persist.vendor.radio.custom_ecc=1 \
     persist.vendor.radio.rat_on=combine \
-    persist.vendor.radio.data_ltd_sys_ind=1 \
-    persist.vendor.radio.data_con_rprt=1 \
-    persist.radio.calls.on.ims=1
+    persist.vendor.radio.sib16_support=1 \
+    ro.telephony.default_network=22,22 \
+    ro.vendor.use_data_netmgrd=true
 
-# Sensors
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    DEVICE_PROVISIONED=1 \
+    persist.sys.fflag.override.settings_network_and_internet_v2=true \
+    ril.subscription.types=NV,RUIM \
+    telephony.lteOnCdmaDevice=1
+
+# Sensor
 PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.sdk.sensors.gestures=false \
+    ro.vendor.sensors.cmc=false \
+    ro.vendor.sensors.facing=false \
     ro.vendor.sensors.dev_ori=true \
     ro.vendor.sensors.pmd=true \
     ro.vendor.sensors.sta_detect=true \
-    ro.vendor.sensors.mot_detect=true \
-    ro.vendor.sensors.dpc=true \
-    ro.vendor.sensors.multishake=true \
-    persist.vendor.sensors.direct_channel=true
+    ro.vendor.sensors.mot_detect=true
 
-# Maintainer
+# Time
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.mk.maintainer=Cyborg2017
+    persist.vendor.delta_time.enable=true
+
+# WFD
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    persist.debug.wfd.enable=1
