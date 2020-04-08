14849efc575e24b9d9348e44b896ccd1df28b82a
Roman Kiryanov <rkir@google.com>
2020-04-08 14:23:47 -0700

    nx611j: Replace audio@2.0-service with audio.service
    Legacy service name, use android.hardware.audio.service instead
    
    Bug: 153579444
    Test: presubmit
    Signed-off-by: Roman Kiryanov <rkir@google.com>
    Merged-In: I4c05accb86dd0c9cd1b3fef3d84e0d54b2afc6a1
    Change-Id: I6b55b4b1db2577f4176c99dca7021cfe78e13307
    (cherry picked from commit 51a3e0ee8434c2a8c2f46329813e9468bfc47533)

--- device.mk
+++ device.mk
@@ -84,8 +84,8 @@ PRODUCT_COPY_FILES += \
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl \
-    android.hardware.audio@2.0-service \
     android.hardware.audio.effect@6.0-impl \
+    android.hardware.audio.service \
     android.hardware.soundtrigger@2.2-impl:32 \
     audio.a2dp.default \
     audio.primary.sdm660 \
