105d36306e3903f62a26959a118be3c69b3706ca
Cyborg2017 <wangyang0987654@gmail.com>
2022-09-05 21:06:41 +0800

    Revert "nx611j: Enforce RRO for frameworks-res overlays"

--- device.mk
+++ device.mk
@@ -9,9 +9,6 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
-PRODUCT_ENFORCE_RRO_TARGETS := \
-    framework-res
-
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
