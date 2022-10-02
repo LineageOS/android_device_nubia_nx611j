febbe614bbd5860ceba0de96e4ec771db990c786
Cyborg2017 <wangyang0987654@gmail.com>
2022-10-02 20:57:13 +0800

    nx611j: Use fstab.qcom as recovery fstab

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -125,7 +125,7 @@ TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
 # Recovery
-TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/recovery/recovery.fstab
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
 
 # RIL
 ENABLE_VENDOR_RIL_SERVICE := true
