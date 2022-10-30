baf37b6443699ddb90dc9c891cbe0db2655f4655
Michael Bestas <mkbestas@lineageos.org>
2022-10-30 19:01:36 +0200

    nx611j: Move qti_whitelist.xml to /system_ext
    Change-Id: Id8617513cfd686b3f6888a89c1b1db196acfb476

--- device.mk
+++ device.mk
@@ -351,7 +351,7 @@ PRODUCT_COPY_FILES += \
 
 # Low power Whitelist
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/qti_whitelist.xml:system/etc/sysconfig/qti_whitelist.xml
+    $(LOCAL_PATH)/configs/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/sysconfig/qti_whitelist.xml
 
 # QMI
 PRODUCT_PACKAGES += \
