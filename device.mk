4f9a57eade61d775efd827d8f9b98abb0ba91246
Cyborg2017 <wangyang0987654@gmail.com>
2023-10-02 20:26:15 +0800

    nx611j: Move hals.conf to vendor tree
    Change-Id: I2f14a302c322f6c94cec146005a40fb292befdd1

--- device.mk
+++ device.mk
@@ -391,10 +391,6 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-service \
     libsensorndkbridge
 
-# Sensor HAL conf file
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
-
 # Tetheroffload
 PRODUCT_PACKAGES += \
     ipacm \
