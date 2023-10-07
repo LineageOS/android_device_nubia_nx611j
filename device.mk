7146a083b929a7b96fa02c255be31ec18f5eb652
me-cafebabe <me.cafebabe@gmail.com>
2023-10-07 17:26:01 +0800

    nx611j: Switch to legacy WiFi HIDL HAL
    * With the new WiFi AIDL HAL, the framework thinks the device
      supports interface combination for some unknown reason,
      even though the service doesn't advertise it. As a result,
      the WiFi service fails on configureChip method.
    
    Change-Id: I0c3fa7b4c947c3525c30f7cef0b2674bee224da7

--- device.mk
+++ device.mk
@@ -415,7 +415,7 @@ PRODUCT_PACKAGES += \
 
 # Wifi
 PRODUCT_PACKAGES += \
-    android.hardware.wifi@1.0-service \
+    android.hardware.wifi@1.0-service.legacy \
     hostapd \
     libwifi-hal-qcom \
     libwpa_client \
