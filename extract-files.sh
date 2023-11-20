66d4c9e2f5f4841710271ad61867eb8bb39d30a7
Bruno Martins <bgcngm@gmail.com>
2023-11-20 15:40:20 +0000

    nx611j: Remove QtiTelephonyServicelibrary
    This is not used and triggers a CtsStrictJavaPackagesTestCases
    failure as follows:
    
      STACKTRACE:
      Classes from protobuf libraries must not be included in bootclasspath and systemserverclasspath without being jarjared.
      expected to be empty
    
    Test: Run `atest CtsStrictJavaPackagesTestCases` and verify that
    testNoProtobufClassesWithoutJarjar does not fail anymore.
    
    Change-Id: Ic81b09d47e490b7a64e3bc8fb2deae8284c44b0d

--- extract-files.sh
+++ extract-files.sh
@@ -58,7 +58,7 @@ function blob_fixup() {
         system_ext/etc/init/dpmd.rc)
             sed -i "s|/system/product/bin/|/system/system_ext/bin/|g" "${2}"
             ;;
-        system_ext/etc/permissions/com.qti.dpmframework.xml | system_ext/etc/permissions/dpmapi.xml | system_ext/etc/permissions/telephonyservice.xml)
+        system_ext/etc/permissions/com.qti.dpmframework.xml | system_ext/etc/permissions/dpmapi.xml)
             sed -i "s|/system/product/framework/|/system/system_ext/framework/|g" "${2}"
             ;;
         system_ext/etc/permissions/qcrilhook.xml)
